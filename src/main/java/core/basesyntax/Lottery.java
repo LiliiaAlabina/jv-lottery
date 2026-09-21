46b735f33c60ae972fe67d81acededaef3847118
Liliia <lil600967@gmail.com>
2026-09-21 16:38:56 +0300

    Fixed MAXIMUM_NUMBER

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,15 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAXIMUM_NUMBER = 100;
 
     private final Random random = new Random();
-    private final int limit = 100;
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
+
         ColorSupplier randomColor = new ColorSupplier();
-        ball.setNumber(random.nextInt(limit));
+        ball.setNumber(random.nextInt(MAXIMUM_NUMBER));
         ball.setColor(randomColor.getRandomColor());
         return ball;
     }
